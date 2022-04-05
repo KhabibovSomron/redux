86dd4861274f7e9d379cd0c29e3549dde293b811
Somron Khabibov <habibov200@gmail.com>
2022-04-05 12:32:11 +0300

    Рефакторинг

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { addCashAction, getCashAction } from './store/cashReducer';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
 
 function App() {
   
   const dispatch = useDispatch()
-  const cash = useSelector(state => state.cash)
+  const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
+  console.log(customers)
   console.log(cash)
 
   const getCash = (cash) => {
-    dispatch({type: "GET_CASH", payload: cash})
+    dispatch(getCashAction(cash))
   }
 
   const addCash = (cash) => {
-      dispatch({type: "ADD_CASH", payload: cash})
+      dispatch(addCashAction(cash))
+  }
+
+  const addCustomer = (name) => {
+    const customer = {
+      name: name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const deleteCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
   }
 
   return (
     <div className="App">
-      <div className='cash'>$ {cash}</div>
-      <button onClick={() => addCash(Number(prompt("Cash: ")))}>Пополнить счет</button>
-      <button onClick={() => getCash(Number(prompt("Cash: ")))}>Снять со счета</button>
+      <div>
+        <div className='cash'>$ {cash}</div>
+        <button onClick={() => addCash(Number(prompt("Cash: ")))}>Пополнить счет</button>
+        <button onClick={() => getCash(Number(prompt("Cash: ")))}>Снять со счета</button>
+        <button onClick={() => addCustomer(prompt("Name: "))}>Добавить клиента</button>
+      </div>
+      {customers.length > 0 ?
+        <div>
+          {customers.map(customer => 
+            <div onClick={() => deleteCustomer(customer)} style={{fontSize:"25px", padding: 10, margin:10, border:"2px solid black", width: "500px"}}>{customer.name}</div>    
+          )}
+        </div>
+        :
+        <div style={{fontSize: "35px", marginTop: "20px"}}>
+          Клиенты отсутствуют!
+        </div>
+      }
     </div>
   );
 }
---
+++ src/store/customerReducer.js
@@ -0,0 +1,19 @@
+const defaultState = {
+    customers: [],
+}
+const ADD_CUSTOMER = "ADD_CUSTOMER"
+const REMOVE_CUSTOMERS = "DELETE_CUSTOMER"
+
+export const customerReducer = (state = defaultState, action) => {
+    switch (action.type) {
+      case "ADD_CUSTOMER":
+        return {...state, customers: [...state.customers, action.payload]}
+      case "DELETE_CUSTOMER":
+        return {...state, customers: state.customers.filter(customer => customer.id !== action.payload)}
+      default:
+        return state
+    }
+  }
+
+  export const addCustomerAction = (payload) => ({type: ADD_CUSTOMER, payload})
+  export const removeCustomerAction = (payload) => ({type: REMOVE_CUSTOMERS, payload})
